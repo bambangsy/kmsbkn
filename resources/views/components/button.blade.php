c1e1604483e15f227702ff8e8c4336255b0747dc
bambangsy <bambangsy28@gmail.com>
2024-04-16 14:28:07 +0700

    add other file

---
+++ resources/views/components/button.blade.php
@@ -0,0 +1,4 @@
+@props(['tulisan' => 'halo', "warna" => "blue"])
+
+<button class="bg-{{$warna}}-500 text-white px-4 py-2 rounded-md">{{$tulisan}}</button>
+
---
+++ resources/views/menu1.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Document</title>
+</head>
+<body>
+    <h1 class="text-3xl font-bold underline">hello world</h1>
+    <x-button tulisan="hi" warna="red" />
+    <x-button  />
+    <x-danger-button>sadjadaskdjas</x-danger-button>
+</body>
+</html>
+
