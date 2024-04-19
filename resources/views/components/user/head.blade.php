f225856721d37f3e472b1ea9b4b8c984ac21b1b3
Raka Dimas Saputra <rakadimas56@gmail.com>
2024-04-19 09:41:38 +0700

    update pemisahan

---
+++ resources/views/components/user/head.blade.php
@@ -0,0 +1,31 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Luma</title>
+
+    <!-- Prevent the demo from appearing in search engines -->
+    <meta name="robots" content="noindex">
+
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700%7CRoboto:400,500%7CExo+2:600&display=swap"
+        rel="stylesheet">
+
+    <!-- Preloader -->
+    <link type="text/css" href="{{ asset('luma/vendor/spinkit.css') }}" rel="stylesheet">
+
+    <!-- Perfect Scrollbar -->
+    <link type="text/css" href="{{ asset('luma/vendor/perfect-scrollbar.css') }}" rel="stylesheet">
+
+    <!-- Material Design Icons -->
+    <link type="text/css" href="{{ asset('luma/css/material-icons.css') }}" rel="stylesheet">
+
+    <!-- Font Awesome Icons -->
+    <link type="text/css" href="{{ asset('luma/css/fontawesome.css') }}" rel="stylesheet">
+
+    <!-- Preloader -->
+    <link type="text/css" href="{{ asset('luma/css/preloader.css') }}" rel="stylesheet">
+
+    <!-- App CSS -->
+    <link type="text/css" href="{{ asset('luma/css/app.css') }}" rel="stylesheet">
+
+</head>
---
+++ resources/views/components/user/script.blade.php
@@ -0,0 +1,21 @@
+    <!-- jQuery -->
+    <script src="{{ asset('luma/vendor/jquery.min.js') }}"></script>
+
+    <!-- Bootstrap -->
+    <script src="{{ asset('luma/vendor/popper.min.js') }}"></script>
+    <script src="{{ asset('luma/vendor/bootstrap.min.js') }}"></script>
+
+    <!-- Perfect Scrollbar -->
+    <script src="{{ asset('luma/vendor/perfect-scrollbar.min.js') }}"></script>
+
+    <!-- DOM Factory -->
+    <script src="{{ asset('luma/vendor/dom-factory.js') }}"></script>
+
+    <!-- MDK -->
+    <script src="{{ asset('luma/vendor/material-design-kit.js') }}"></script>
+
+    <!-- App JS -->
+    <script src="{{ asset('luma/js/app.js') }}"></script>
+
+    <!-- Preloader -->
+    <script src="{{ asset('luma/js/preloader.js') }}"></script>
