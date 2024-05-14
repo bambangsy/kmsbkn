4ed6c0c2c56d89ce55569b91b4e2f9d0760ec1b2
Raka Dimas Saputra <rakadimas56@gmail.com>
2024-05-14 15:42:34 +0700

    improve the admin frontpage and user management FE

--- resources/views/admin/frontpage_management/frontpage_management.blade.php
+++ resources/views/admin/frontpage_management/frontpage_management.blade.php
@@ -2,126 +2,128 @@
 
 
 @section('content')
-<div class="container mt-5">
-    <div class="row justify-content-center">
-        <div>
-            <div class="col-md-8">
-                <h1 class="text-3xl font-bold">Manajemen Front Page</h1>
-                <h2 class="text-2xl mt-3">Highlight Knowledge</h2>
-                <form method="GET" action="{{ route('admin.frontpage_management.create') }}">
-                    @csrf
-                    <button type="submit" name="value" value="knowledge" readonly>Add</button>
-                </form>
-                
-            <table>
-                <thead>
-                    <tr>
-                        <th>Order</th>
-                        <th>Name</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($knowledges as $knowledge)
-                    <tr>
-                        <td>{{ $knowledge->order }}</td>
-                        <td>{{ $knowledge->name }}</td>
-                        <td><a href="#">Edit</a></td>
-                        <td>
-                            <form method="POST" action="{{ route('admin.frontpage_management.destroy', $knowledge->id) }}">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-danger" name= "deleted_value" value = "knowledge">Hapus</button>
-                            </form>
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
+<div class="container mx-auto">
+    <div class="grid grid-cols-1 gap-8 justify-center">
+        <!-- Highlight Knowledge Section -->
+        <div class="col-md-8 bg-white dark:bg-gray-800 rounded-lg p-6">
+            <h1 class="text-3xl font-bold">Manajemen Front Page</h1>
+            <h2 class="text-2xl mt-3">Highlight Knowledge</h2>
+            <form method="GET" action="{{ route('admin.frontpage_management.create') }}" class="mt-3">
+                @csrf
+                <button type="submit" name="value" value="knowledge" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Add</button>
+            </form>
+
+            <div class="overflow-x-auto mt-5">
+                <table class="min-w-full bg-white dark:bg-gray-800">
+                    <thead>
+                        <tr>
+                            <th class="px-4 py-2">Order</th>
+                            <th class="px-4 py-2">Name</th>
+                            <th class="px-4 py-2">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($knowledges as $knowledge)
+                        <tr class="border-b">
+                            <td class="px-4 py-2">{{ $knowledge->order }}</td>
+                            <td class="px-4 py-2">{{ $knowledge->name }}</td>
+                            <td class="px-4 py-2">
+                                <a href="#" class="text-blue-500 hover:text-blue-700">Edit</a>
+                            </td>
+                            <td>
+                                <form method="POST" action="{{ route('admin.frontpage_management.destroy', $knowledge->id) }}">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded" name="deleted_value" value="knowledge">Hapus</button>
+                                </form>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
-
-        </div>
-        
-
-
-        <div>
-            <div class="col-md-8">
-                <h1 class="text-3xl font-bold">Manajemen Front Page</h1>
-                <h2 class="text-2xl mt-3">Highlight Alur</h2>
-                <form method="GET" action="{{ route('admin.frontpage_management.create') }}">
-                    @csrf
-                    <button type="submit" name="value" value="path" readonly>Add</button>
-                </form>
-                
-            <table>
-                <thead>
-                    <tr>
-                        <th>Order</th>
-                        <th>Name</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($paths as $path)
-                    <tr>
-                        <td>{{ $path->order }}</td>
-                        <td>{{ $path->name }}</td>
-                        <td><a href="#">Edit</a></td>
-                    <td>
-                        <form method="POST" action="{{ route('admin.frontpage_management.destroy', $path->id) }}">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger" name= "deleted_value" value = "path">Hapus</button>
-                        </form>
-                    </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
-            </div>
-
         </div>
 
+        <!-- Highlight Path Section -->
+        <div class="col-md-8 bg-white dark:bg-gray-800  rounded-lg p-6">
+            <h1 class="text-3xl font-bold">Manajemen Front Page</h1>
+            <h2 class="text-2xl mt-3">Highlight Path</h2>
+            <form method="GET" action="{{ route('admin.frontpage_management.create') }}" class="mt-3">
+                @csrf
+                <button type="submit" name="value" value="path" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Add</button>
+            </form>
 
-
-
-        <div>
-            <div class="col-md-8">
-                <h1 class="text-3xl font-bold">Manajemen Front Page</h1>
-                <h2 class="text-2xl mt-3">Highlight Course</h2>
-                <form method="GET" action="{{ route('admin.frontpage_management.create') }}">
-                    @csrf
-                    <button type="submit" name="value" value="course" readonly>Add</button>
-                </form>
-                
-            <table>
-                <thead>
-                    <tr>
-                        <th>Order</th>
-                        <th>Name</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($courses as $course)
-                    <tr>
-                        <td>{{ $course->order }}</td>
-                        <td>{{ $course->name }}</td>
-                        <td><a href="#">Edit</a></td>
-                        <td>
-                            <form method="POST" action="{{ route('admin.frontpage_management.destroy', $course->id) }}">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-danger" name= "deleted_value" value = "course">Hapus</button>
-                            </form>
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
+            <div class="overflow-x-auto mt-5">
+                <table class="min-w-full bg-white dark:bg-gray-800">
+                    <thead>
+                        <tr>
+                            <th class="px-4 py-2">Order</th>
+                            <th class="px-4 py-2">Name</th>
+                            <th class="px-4 py-2">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($paths as $path)
+                        <tr class="border-b">
+                            <td class="px-4 py-2">{{ $path->order }}</td>
+                            <td class="px-4 py-2">{{ $path->name }}</td>
+                            <td class="px-4 py-2">
+                                <a href="#" class="text-blue-500 hover:text-blue-700">Edit</a>
+                            </td>
+                            <td>
+                                <form method="POST" action="{{ route('admin.frontpage_management.destroy', $path->id) }}">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded" name="deleted_value" value="path">Hapus</button>
+                                </form>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
+        </div>
 
+        <!-- Highlight Course Section -->
+        <div class="col-md-8 bg-white dark:bg-gray-800 rounded-lg p-6">
+            <h1 class="text-3xl font-bold">Manajemen Front Page</h1>
+            <h2 class="text-2xl mt-3">Highlight Course</h2>
+            <form method="GET" action="{{ route('admin.frontpage_management.create') }}" class="mt-3">
+                @csrf
+                <button type="submit" name="value" value="course" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Add</button>
+            </form>
+
+            <div class="overflow-x-auto mt-5">
+                <table class="min-w-full bg-white dark:bg-gray-800">
+                    <thead>
+                        <tr>
+                            <th class="px-4 py-2">Order</th>
+                            <th class="px-4 py-2">Name</th>
+                            <th class="px-4 py-2">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($courses as $course)
+                        <tr class="border-b">
+                            <td class="px-4 py-2">{{ $course->order }}</td>
+                            <td class="px-4 py-2">{{ $course->name }}</td>
+                            <td class="px-4 py-2">
+                                <a href="#" class="text-blue-500 hover:text-blue-700">Edit</a>
+                            </td>
+                            <td>
+                                <form method="POST" action="{{ route('admin.frontpage_management.destroy', $course->id) }}">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded" name="deleted_value" value="course">Hapus</button>
+                                </form>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </div>
+
 @endsection
--- resources/views/admin/user_management/user_management.blade.php
+++ resources/views/admin/user_management/user_management.blade.php
@@ -2,125 +2,115 @@
 
 
 @section('content')
-    <div class="container mt-5">
-        <div class="row justify-content-center">
-            <div>
-                <div class="col-md-8">
-                    <h1 class="text-3xl font-bold">Manajemen User</h1>
-                    <form method="GET" action="{{ route('admin.user_management.create') }}">
-                        @csrf
-                        <button type="submit" name="value" value="user">Tambah</button>
-                    </form>
-
-                    <div class="mt-5">
-                        <label for="filter" class="block font-medium text-sm text-gray-700 dark:text-white">
-                            Filter Validasi:</label>
-
-                        <select id="filter" name="filter"
-                            class="form-select mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            onchange="window.location.href = this.value">
-                            <option value="{{ route('admin.user_management', ['filter' => 'sudah_divalidasi']) }}" @if($filter == 'sudah_divalidasi') selected @endif>
-                                Sudah Divalidasi
-                            </option>
-                            <option value="{{ route('admin.user_management', ['filter' => 'belum_divalidasi']) }}" @if($filter == 'belum_divalidasi') selected @endif>
-                                Belum Divalidasi
-                            </option>
-                        </select>
 
 
-                        <script>
-                            function filterSelection() {
-                                var filter = document.getElementById("filter").value;
-                                console.log(filter);
-
-
-
-                                // Logika untuk penyaringan berdasarkan nilai yang dipilih
-                            }
-                        </script>
-
-
-                    </div>
-                    <table class="mt-5">
+    <div class="container mx-auto">
+            <div class="bg-white dark:bg-gray-800 rounded-lg p-6">
+                <h1 class="text-3xl font-bold mb-6">Manajemen User</h1>
+                <form method="GET" action="{{ route('admin.user_management.create') }}">
+                    @csrf
+                    <button type="submit" name="value" value="user"
+                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        Tambah
+                    </button>
+                </form>
+                
+                <div class="mt-5">
+                    <label for="filter" class="block font-medium text-sm text-gray-700 dark:text-white">
+                        Filter Validasi:</label>
+                    <select id="filter" name="filter"
+                        class="form-select mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        onchange="window.location.href = this.value">
+                        <option value="{{ route('admin.user_management', ['filter' => 'sudah_divalidasi']) }}" @if($filter == 'sudah_divalidasi') selected @endif>
+                            Sudah Divalidasi
+                        </option>
+                        <option value="{{ route('admin.user_management', ['filter' => 'belum_divalidasi']) }}" @if($filter == 'belum_divalidasi') selected @endif>
+                            Belum Divalidasi
+                        </option>
+                    </select>
+                </div>
+                
+                <div class="overflow-x-auto mt-5">
+                    <table class="min-w-full bg-white dark:bg-gray-800">
                         <thead>
                             <tr>
-                                <th>Nama</th>
-                                <th>Email</th>
-                                <th>Status</th>
-                                <th>Action</th>
+                                <th class="px-4 py-2">Nama</th>
+                                <th class="px-4 py-2">Email</th>
+                                <th class="px-4 py-2">Status</th>
+                                <th class="px-4 py-2">Action</th>
                             </tr>
                         </thead>
                         <tbody>
                             @if(request('filter') === 'sudah_divalidasi')
                                 @foreach ($validated_users as $validated_user)
-                                
-                                        <tr>
-                                            <td>{{ $validated_user->name }}</td>
-                                            <td>{{ $validated_user->email }}</td>
-                                            <td>Tervalidasi</td>
-                                            <td class="whitespace-nowrap py-2 flex justify-end">
-                                                <form action="{{ route('admin.user_management.accept', ['id' => $validated_user->id, 'filter' => $filter]) }}"
-                                                    method="POST">
-                                                    @csrf
-                                                    @method('PATCH')
-                                                    <button type="submit">
-                                                        Terima</button>
-                                                </form>
-                                                <form action="{{ route('admin.user_management.declined', ['id' => $validated_user->id, 'filter' =>  $filter]) }}"
-                                                    method="POST">
-                                                    @csrf
-                                                    @method('PATCH')
-                                                    <button type="submit">
-                                                        Tolak</button>
-                                                </form>
-                                                <form action="{{ route('admin.user_management.destroy', ['id' => $validated_user->id, 'filter' => $filter]) }}" method="POST">
-                                                    @csrf
-                                                    @method('DELETE')
-                                                    <button type="submit">
-                                                        Hapus</button>
-                                                </form>
-                                            </td>
-                                        </tr>
-                                    
+                                    <tr class="border-b">
+                                        <td class="px-4 py-2">{{ $validated_user->name }}</td>
+                                        <td class="px-4 py-2">{{ $validated_user->email }}</td>
+                                        <td class="px-4 py-2">Tervalidasi</td>
+                                        <td class="px-4 py-2 flex space-x-2">
+                                            <form action="{{ route('admin.user_management.accept', ['id' => $validated_user->id, 'filter' => $filter]) }}" method="POST">
+                                                @csrf
+                                                @method('PATCH')
+                                                <button type="submit" class="bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-2 rounded">
+                                                    Terima
+                                                </button>
+                                            </form>
+                                            <form action="{{ route('admin.user_management.declined', ['id' => $validated_user->id, 'filter' =>  $filter]) }}" method="POST">
+                                                @csrf
+                                                @method('PATCH')
+                                                <button type="submit" class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-1 px-2 rounded">
+                                                    Tolak
+                                                </button>
+                                            </form>
+                                            <form action="{{ route('admin.user_management.destroy', ['id' => $validated_user->id, 'filter' => $filter]) }}" method="POST">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded">
+                                                    Hapus
+                                                </button>
+                                            </form>
+                                        </td>
+                                    </tr>
                                 @endforeach
                             @endif
                             @if(request('filter') === 'belum_divalidasi' || request('filter') === null)
-                            @foreach ($not_validated_users as $not_validated_user)
-                                    
-                                <tr>
-                                    <td>{{ $not_validated_user->name }}</td>
-                                    <td>{{ $not_validated_user->email }}</td>
-                                    <td>Belum Tervalidasi</td>
-                                    <td class="whitespace-nowrap py-2 flex justify-end">
-                                        <form action="{{ route('admin.user_management.accept', [$not_validated_user->id,'filter' => $filter]) }}"
-                                            method="POST">
-                                            @csrf
-                                            @method('PATCH')
-                                            <button type="submit">
-                                                Terima</button>
-                                        </form>
-                                        <form action="{{ route('admin.user_management.declined', [$not_validated_user->id,'filter' => $filter]) }}"
-                                            method="POST">
-                                            @csrf
-                                            @method('PATCH')
-                                            <button type="submit">
-                                                Tolak</button>
-                                        </form>
-                                        <form action="{{ route('admin.user_management.destroy', [$not_validated_user->id,'filter' => $filter]) }}"
-                                            method="DELETE">
-                                            @csrf
-                                            <button type="submit">
-                                                Hapus</button>
-                                        </form>
-                                    </td>
-                                </tr>
-                            
-                            @endforeach
+                                @foreach ($not_validated_users as $not_validated_user)
+                                    <tr class="border-b">
+                                        <td class="px-4 py-2">{{ $not_validated_user->name }}</td>
+                                        <td class="px-4 py-2">{{ $not_validated_user->email }}</td>
+                                        <td class="px-4 py-2">Belum Tervalidasi</td>
+                                        <td class="px-4 py-2 flex space-x-2">
+                                            <form action="{{ route('admin.user_management.accept', [$not_validated_user->id, 'filter' => $filter]) }}" method="POST">
+                                                @csrf
+                                                @method('PATCH')
+                                                <button type="submit" class="bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-2 rounded">
+                                                    Terima
+                                                </button>
+                                            </form>
+                                            <form action="{{ route('admin.user_management.declined', [$not_validated_user->id, 'filter' => $filter]) }}" method="POST">
+                                                @csrf
+                                                @method('PATCH')
+                                                <button type="submit" class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-1 px-2 rounded">
+                                                    Tolak
+                                                </button>
+                                            </form>
+                                            <form action="{{ route('admin.user_management.destroy', [$not_validated_user->id, 'filter' => $filter]) }}" method="POST">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded">
+                                                    Hapus
+                                                </button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
                             @endif
                         </tbody>
                     </table>
                 </div>
             </div>
         </div>
-    </div>
+
+
+
 @endsection
