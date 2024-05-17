d6082e9cba06900ec58fa92b9e8275e6dde2a6b4
bambangsy <bambangsy28@gmail.com>
2024-05-17 16:51:51 +0700

    fix some bugs

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,9 +16,9 @@
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            <div>
+            <div class="mb-3">
                 <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                    <img src="https://upload.wikimedia.org/wikipedia/id/e/ea/Logo_BKN_Baru.png" alt="bkn logo" width="100px">
                 </a>
             </div>
 
